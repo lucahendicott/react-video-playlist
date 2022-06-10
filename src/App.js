18297cf0ab3ae71032afacc18fcb0fb0c3100da7
Lucah Endicott <lucahdavid88@gmail.com>
2022-06-09 17:48:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const data = {
+  tutorialVideos: [
+    {
+      id: 1,
+      title: "RightOn! Overview",
+      src: "https://www.youtube.com/embed/sUlnToE2bqQ",
+    },
+    {
+      id: 2,
+      title: "Host & Display Overview",
+      src: "https://www.youtube.com/embed/IcrEa8pa0K8",
+    },
+  ],
+};
 
 function App() {
+  const [currentVideo, setCurrentVideo] = useState(
+    data.tutorialVideos[0].src,
+    data.tutorialVideos[0].title
+  );
+
+  const handleClick1 = () => {
+    setCurrentVideo(data.tutorialVideos[0].src, data.tutorialVideos[0].title);
+  };
+  const handleClick2 = () => {
+    setCurrentVideo(data.tutorialVideos[1].src, data.tutorialVideos[1].title);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="tutorial-video-panel">
+        <h4 className="tutorial-videos-header-text">Tutorial Videos</h4>
+        <div className="tutorial-videos-panel">
+          <button
+            onClick={() => handleClick1()}
+            className="tutorial-videos-text"
+          >
+            {data.tutorialVideos[0].title}
+          </button>
+          <br />
+          <br />
+          <button
+            onClick={() => handleClick2()}
+            className="tutorial-videos-text"
+          >
+            {data.tutorialVideos[1].title}
+          </button>
+        </div>
+      </div>
+      <br />
+      <br />
+      <iframe
+        width="860"
+        height="483"
+        src={currentVideo}
+        title={currentVideo}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
